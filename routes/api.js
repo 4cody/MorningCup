491ca21742d4d8f752e5df8afd8aba3ed2d47357
Cody Shaner <cshaner04@gmail.com>
2020-10-21 18:39:45 -0500

    deploy

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios')
 
-router.get('/api/news', async (req, res) => {
+router.get('/news', async (req, res) => {
     try {
       const nRes = await axios('https://newsapi.org/v2/top-headlines?country=us&pageSize=15&apiKey=e5da89b57ee347a1a1da306427dc5fa7')
   
@@ -12,7 +12,7 @@ router.get('/api/news', async (req, res) => {
     }
 })
   
-router.get('/api/weather', async (req, res) => {
+router.get('/weather', async (req, res) => {
     try {
         const wRes = await axios('https://api.weatherbit.io/v2.0/current?postal_code=54880&country=US&units=i&key=c3a3ccff8c144a68b378d4178a432076')
 
